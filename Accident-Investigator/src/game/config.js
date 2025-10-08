7c4aa93abb3d7408c70fa02a0e338313c32cc5ed
Kusum Agrawal <kusum.agrawal.0202@gmail.com>
2025-10-08 19:47:24 +0530

    minor UI update

--- Accident-Investigator/src/game/config.js
+++ Accident-Investigator/src/game/config.js
@@ -1,14 +1,30 @@
 import Phaser from "phaser";
 import BootScene from "./scenes/BootScene.js";
 import CaseScene from "./scenes/CaseScene.js";
-import ResultScene from "./scenes/ResultScene.js";
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1024,            // logical width (for layout calculations)
+  height: 768,            // logical height
   backgroundColor: "#000000",
-  parent: "game-container",   // This div will be in React
+  parent: "game-container",
+
+  //Key scaling options
+  scale: {
+    mode: Phaser.Scale.FIT,   // makes canvas fit the parent container
+    autoCenter: Phaser.Scale.CENTER_BOTH, // centers it
+    width: 1024,
+    height: 768,
+    min: {
+      width: 800,
+      height: 600
+    },
+    max: {
+      width: 1600,
+      height: 1200
+    }
+  },
+
   scene: [BootScene, CaseScene]
 };
 
